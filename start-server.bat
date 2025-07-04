e629b6643bf1258a38d1ef1c7b40c1bda6661bd2
EdsonJHL <edsonjhuallpal@gmail.com>
2025-07-03 19:26:34 -0500

    Cambio de prueba para Jenkins

--- start-server.bat
+++ start-server.bat
@@ -1,5 +1,8 @@
 @echo off
-cd /d C:\ProgramData\Jenkins\.jenkins\workspace\hotel-webapp
+echo Cerrando procesos en el puerto 3000...
+for /f "tokens=5" %%a in ('netstat -aon ^| findstr :3000') do taskkill /F /PID %%a
+
+cd /d %~dp0
 set NODE_OPTIONS=--openssl-legacy-provider
-call npm start
+npm start
 pause
